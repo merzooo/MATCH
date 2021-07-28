a3b0a3c20f509797f1bb27199e3f42f52a839ff4
merzooo <mlakfezr@gmail.com>
2021-07-29 00:22:49 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd81765b8e39428796d616fcdd32add1b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ed4fb922-1117-476d-b2a7-045d1b96a239'
+};
